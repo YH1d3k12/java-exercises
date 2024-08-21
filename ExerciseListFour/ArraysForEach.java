948153f7138d7895f77f7da5a90fd976761297f0
Djalma <djalmahy@hotmail.com>
2024-08-20 21:05:22 -0300

    Exercicio 2 Lista 4

---
+++ ExerciseListFour/ArraysForEach.java
@@ -0,0 +1,19 @@
+/* 
+    -----| Exercício 02 |-----
+    2) Crie um programa que imprima um array com Foreach. 
+*/
+package ExerciseListFour;
+
+public class ArraysForEach {
+    public static void main(String[] args) {
+        // Array vázio, definido como tamanho 10.
+        // int[] arrayInteiros = new int[10];
+
+        // Array populado com dados.
+        int[] arrayInteiros = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        for (int i : arrayInteiros) {
+            System.out.println("Valor: " + i);
+        }
+    }
+}
